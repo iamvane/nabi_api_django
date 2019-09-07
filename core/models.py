1b1b09704b6c1b878ae929538d83e178aea850fd
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-09-07 10:12:37 -0400

    [NABI-35] fixed issue with creating superuser

--- core/models.py
+++ core/models.py
@@ -3,6 +3,33 @@ from django.db import models
 
 from .constants import *
 
+class UserManager(BaseUserManager):
+    """
+    A custom user manager to deal with emails as unique identifiers for auth
+    instead of usernames. The default that's used is "UserManager"
+    """
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
 
 class UserManager(BaseUserManager):
     """Define required methods for custom User model usage."""
@@ -37,6 +64,7 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True)
     username = models.CharField(blank=True, default='', max_length=120)
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
