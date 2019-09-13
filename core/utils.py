ed8898485462ab29b06e14dfeb5a6cfc95f804b2
Luis Llave <llaveluis@gmail.com>
2019-09-12 20:55:05 -0500

    Add endpoints to forgot-password: to send email for reset password and to set a new one

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from hashlib import sha1
+
+
 def update_model(instance, **kwargs):
     for k, v in kwargs.items():
         if __has_field(instance, k):
@@ -10,3 +14,11 @@ def __has_field(instance, name):
         if field.name == name:
             return True
     return False
+
+
+def generate_hash(value):
+    """Generate an unique hash."""
+    now = datetime.utcnow()
+    text = '{}{}'.format(value, now.microsecond)
+    hash_value = sha1(text.encode())
+    return hash_value.hexdigest()
