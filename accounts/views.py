d76d6ab3ef346d80231ff65d8769326f1b339e00
Luis <llaveluis@gmail.com>
2019-09-15 20:26:54 -0500

    Implement forgot-password

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,34 @@
+from datetime import timedelta
+from logging import getLogger
 from twilio.rest import Client
 
 from django.conf import settings
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
-from django.db import transaction
+from django.core.mail import EmailMultiAlternatives
+from django.db import transaction, IntegrityError
+from django.db.models import ObjectDoesNotExist
 from django.middleware.csrf import get_token
+from django.template import loader
 from django.utils import timezone
 
 from rest_framework import views, status
 from rest_framework.permissions import *
 from rest_framework.response import Response
 
+from core.models import UserToken
+from core.utils import generate_hash
+
 from .models import Instructor, Parent, PhoneNumber, Student, get_user_phones
 from .serializers import (
     InstructorAccountInfoSerializer, InstructorAccountStepTwoSerializer, InstructorCreateAccountSerializer,
-    InstructorProfileSerializer, ParentCreateAccountSerializer, StudentCreateAccountSerializer,
-    ROLE_INSTRUCTOR, ROLE_PARENT
+    InstructorProfileSerializer, ParentCreateAccountSerializer, StudentCreateAccountSerializer, UserEmailSerializer,
+    UserPasswordSerializer, ROLE_INSTRUCTOR, ROLE_PARENT
 )
+
+User = get_user_model()
+logger = getLogger('api_errors')
 
 
 def get_user_response(user_cc):
@@ -83,6 +95,61 @@ class LoginView(views.APIView):
             return Response(get_user_response(get_user(user)))
         else:
             return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPasswordView(views.APIView):
+    """For set a new password, when user forgot it."""
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        serializer = UserEmailSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.data['email']
+            user = User.objects.get(email=email)
+            subject = "Reset Password"
+            email_template_name = 'accounts/email_reset_password.html'
+            repeated_token = True
+            while repeated_token:
+                token = generate_hash(email)
+                expired_time = timezone.now() + timedelta(days=1)
+                try:
+                    UserToken.objects.create(user=user, token=token, expired_at=expired_time)
+                except IntegrityError:
+                    pass
+                else:
+                    repeated_token = False
+            target_url = '{}/forgot-password?token={}'.format(settings.HOSTNAME_PROTOCOL, token)
+            context = {'full_name': user.get_full_name(), 'url_reset_pass': target_url}
+            body = loader.render_to_string(email_template_name, context)
+            email_message = EmailMultiAlternatives(subject, body, settings.DEFAULT_FROM_EMAIL, [email, ])
+            html_email = loader.render_to_string(email_template_name, context)
+            email_message.attach_alternative(html_email, 'text/html')
+            try:
+                email_message.send()
+            except Exception as e:
+                logger.error(e)
+        return Response({'message': 'success'}, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        serializer = UserPasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            token = request.query_params.get('token')
+            if token:
+                try:
+                    user_token = UserToken.objects.get(token=token)
+                except ObjectDoesNotExist:
+                    return Response({'message': 'Wrong token value'}, status=status.HTTP_400_BAD_REQUEST)
+                if user_token.expired_at < timezone.now():
+                    return Response({'message': 'Wrong token value'}, status=status.HTTP_400_BAD_REQUEST)
+                passw = serializer.data['password']
+                user_token.user.set_password(passw)
+                user_token.user.save()
+                user_token.delete()
+                return Response({'message': 'Password set successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Token value is missing'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LogoutView(views.APIView):
