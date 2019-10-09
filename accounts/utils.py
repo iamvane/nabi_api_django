412592ec18c08bf59be8b8ca2911758f44e18854
iamvane <vane.charles24@gmail.com>
2019-10-09 11:17:58 -0400

    [NM-25] implemented welcome email design (#21)
    * [NM-25] implemented welcome email design
    
    * [NM-25] changed doc title
    
    * [NM-25] fixed typo
    
    * [NM-25] added linespace
    
    * [NM-25] removed loggin
    
    * [NM-25] added sentry config
    
    * [NM-25] added sentry module to requirements.txt
    
    * [NM-25] integrated Sentry
    
    * [NM-25] addressed feedback: removed unused variable, unecesasryly declared variable, fixed typo, removed logger
    
    * [NM-25] addressed feedback: moved try block to CreateAccount view

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,6 +1,34 @@
+from django.conf import settings
+from django.template import loader
+from django.core.mail import EmailMultiAlternatives
+
 def init_kwargs(model, arg_dict):
     return {
         k: v for k, v in arg_dict.items() if k in [
             f.name for f in model._meta.get_fields()
         ]
     }
+
+
+def send_welcome_email(user_cc):
+    user = user_cc.user
+    role = user.get_role()
+    referral_token = user.referral_token
+    email = user.email
+    referral_link = '{}/registration?token={}'.format(settings.HOSTNAME_PROTOCOL,
+        referral_token)
+
+    if role == 'instructor':
+        text = "Invite your colleagues to join Nabi and you and them will get a lesson FREE of fees!"
+    else:
+        text = "Invite people you know to join Nabi and you and them will get a FREE lesson!"
+
+    context = {'referral_link': referral_link, 'referral_text': text }
+    text_content = loader.render_to_string('welcome_to_nabi_plain.html', context)
+    html_content = loader.render_to_string('welcome_to_nabi.html', context)
+    from_email = 'Nabi Music <' + settings.DEFAULT_FROM_EMAIL + '>'
+    subject = 'Welcome to Nabi Music!'
+    email_message = EmailMultiAlternatives(subject, text_content, from_email, [email])
+    email_message.attach_alternative(html_content, 'text/html')
+    email_message.send()
+
--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -93,33 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-logs_path = os.path.join(os.path.dirname(BASE_DIR), 'logs')
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-    },
-    'handlers': {
-        'logfile': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(logs_path, "error.log"),
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'api_errors': {
-            'handlers': ['logfile', ],
-            'level': 'ERROR',
-            'propagate': False,
-        }
-    },
-}
-
 HOSTNAME = "www.nabimusic.com"
 HOSTNAME_PROTOCOL = "http://" + HOSTNAME
 
@@ -169,3 +142,12 @@ if not DEBUG:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
         'rest_framework.renderers.JSONRenderer',
     )
+
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://e7aee34ab87d4e62ac4570f9c384436c@sentry.io/1774495",
+    integrations=[DjangoIntegration()]
+)
