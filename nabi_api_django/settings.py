dfa117010b1d3345f50ecd0e0264f9d30acb165f
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-10-09 09:33:40 -0400

    [NM-25] added sentry config

--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -142,3 +142,11 @@ if not DEBUG:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
         'rest_framework.renderers.JSONRenderer',
     )
+
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn="https://e7aee34ab87d4e62ac4570f9c384436c@sentry.io/1774495",
+        integrations=[DjangoIntegration()]
+    )
