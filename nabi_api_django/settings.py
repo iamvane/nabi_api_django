cc560e03d31303d4d9f1d02d397c4f429ce929a9
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-10-13 18:21:04 -0400

    changes in configuratiom

--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -31,8 +31,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
