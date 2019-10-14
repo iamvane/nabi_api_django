d687e2888e0677e997bd8e4aca851ad5c81dc97c
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-10-13 22:31:26 -0400

    more changes

--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'foo' # os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 INSTALLED_APPS = [
