823b99b81d5fde4240a3fe0f545b9e884f4ea056
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-10-13 23:00:19 -0400

    added fix

--- nabi_api_django/production_settings/__init__.py
+++ nabi_api_django/production_settings/__init__.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 import os
 import dj_database_url
+=======
+from nabi_api_django.settings import *
+>>>>>>> 4510ddf... added fix
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
