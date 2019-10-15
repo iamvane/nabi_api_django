295ce065dd75085bced455e1aa781172192fb62f
iamvane <vane.charles24@gmail.com>
2019-10-15 10:33:53 -0400

    [NM-37] implemented referral invitation email design (#29)
    * [NM-37] implemented referral invitation email design
    
    * [NM-37] fixed typo

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.template import loader
 from django.core.mail import EmailMultiAlternatives
+from django.utils import timezone
+
+from core.utils import get_date_a_month_later, send_email
+from core.constants import ROLE_INSTRUCTOR
 
 def init_kwargs(model, arg_dict):
     return {
@@ -14,7 +18,7 @@ def send_welcome_email(user_cc):
     user = user_cc.user
     role = user.get_role()
     referral_token = user.referral_token
-    email = user.email
+    to_email = user.email
     referral_link = '{}/registration?token={}'.format(settings.HOSTNAME_PROTOCOL,
         referral_token)
 
@@ -28,7 +32,48 @@ def send_welcome_email(user_cc):
     html_content = loader.render_to_string('welcome_to_nabi.html', context)
     from_email = 'Nabi Music <' + settings.DEFAULT_FROM_EMAIL + '>'
     subject = 'Welcome to Nabi Music!'
-    email_message = EmailMultiAlternatives(subject, text_content, from_email, [email])
+    email_message = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
     email_message.attach_alternative(html_content, 'text/html')
     email_message.send()
 
+def send_referral_invitation_email(user, email):
+    role = user.get_role()
+    referral_token = user.referral_token
+    first_name = user.first_name
+    last_name = user.last_name
+    date_limit = get_date_a_month_later(timezone.now())
+    to_email = email
+    from_email = 'Nabi Music <' + settings.DEFAULT_FROM_EMAIL + '>'
+    template = 'referral_email.html'
+    plain_template = 'referral_email_plain.html'
+    user_full_name =  '{} {}'.format(first_name, last_name)
+    anonymous_message = ''
+
+    if not user_full_name.strip():
+        if role == ROLE_INSTRUCTOR:
+            subject = 'Teach a lesson FREE of fees'
+            anonymous_message = 'You can teach a music lesson FREE of fees!'
+        else:
+            subject = 'You got a FREE music lesson'
+            anonymous_message = 'You received a FREE music lesson!'
+    else:
+        subject = user_full_name + ' invited you to Nabi Music'
+
+    if role == ROLE_INSTRUCTOR:
+        referral_url = '{}/registration-instructor?token={}'.format(settings.HOSTNAME_PROTOCOL,
+            referral_token)
+        heading = 'sent you a lesson FREE of fees!'
+        description = 'to keep 100% of your earnings.'
+    else:
+        referral_url =  '{}/registration-student?token={}'.format(settings.HOSTNAME_PROTOCOL,
+            referral_token)
+        heading = 'sent you a FREE music lesson!'
+        description = 'to get your FREE lesson.'
+
+    params = {'first_name': first_name, 'last_name': last_name, 'date_limit': date_limit,
+        'referral_url': referral_url, 'heading': heading, 'description': description}
+
+    if anonymous_message:
+        params['anonymous_message']: anonymous_message
+
+    send_email(from_email, [to_email], subject, template, params)
--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ from .serializers import (
     StudentCreateAccountSerializer, StudentDetailsSerializer, TiedStudentSerializer, TiedStudentCreateSerializer,
     UserEmailSerializer, UserInfoUpdateSerializer, UserPasswordSerializer,
 )
-from .utils import send_welcome_email
+from .utils import send_welcome_email, send_referral_invitation_email
 
 
 User = get_user_model()
@@ -453,21 +453,17 @@ class ReferralInvitation(views.APIView):
     def post(self, request):
         serializer = GuestEmailSerializer(data=request.data)
         if serializer.is_valid():
-            params = {'first_name': request.user.first_name, 'last_name': request.user.last_name,
-                      'date_limit': get_date_a_month_later(timezone.now())}
-            if request.user.get_role() == ROLE_INSTRUCTOR:
-                params['referral_url'] = '{}/registration-instructor?token={}'.format(settings.HOSTNAME_PROTOCOL,
-                                                                                      request.user.referral_token)
-                send_email(settings.DEFAULT_FROM_EMAIL, [serializer.validated_data['email'], ],
-                           request.user.get_full_name() + ' invited to Nabimusic',
-                           'core/referral_email_instructor.html', params)
-            else:
-                params['referral_url'] = '{}/registration-student?token={}'.format(settings.HOSTNAME_PROTOCOL,
-                                                                                   request.user.referral_token)
-                send_email(settings.DEFAULT_FROM_EMAIL, [serializer.validated_data['email'], ],
-                           request.user.get_full_name() + ' invited to Nabimusic',
-                           'core/referral_email_student.html', params)
-
+            user = request.user
+            email = serializer.validated_data['email']
+            try:
+                send_referral_invitation_email(user, email)
+            except Exception as e:
+                return Response({
+                    "error": str(e)
+                }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": 'Invitation sent successfully.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class StudentDetailView(views.APIView):
 
