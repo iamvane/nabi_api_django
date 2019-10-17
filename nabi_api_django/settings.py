8a25e6518a1fd68a0616ddf3efc854b6d4e17b87
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-10-17 16:27:36 -0400

    added allowed host

--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
