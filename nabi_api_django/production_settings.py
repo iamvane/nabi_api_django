8671dda582b970bead8ab3c47203671d57f996d9
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-11-06 09:59:50 -0500

    [NO-TICKET] setp prod installed aps

--- nabi_api_django/production_settings.py
+++ nabi_api_django/production_settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django.contrib.gis',
 
     'debug_toolbar',
     'rest_framework',
--- nabi_api_django/production_settings/__init__.py
+++ nabi_api_django/production_settings/__init__.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django.contrib.gis',
 
     'debug_toolbar',
     'rest_framework',
