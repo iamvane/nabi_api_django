dbddf4faf3f4b313d1fb212f0be3ec3e8f2d7caf
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-11-06 09:30:37 -0500

    [NO-TICKET] changed db in prod settings files

--- nabi_api_django/production_settings.py
+++ nabi_api_django/production_settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'nabi_api_django.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.environ.get('DB_NAME', 'nabidb'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASS', ''),
