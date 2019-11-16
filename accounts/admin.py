59f68b003b6b6315cc2e1a06b06ddb370487239d
Luis Llave <llaveluis@gmail.com>
2019-11-16 16:45:47 -0500

    [NM-106] Add interviewed field to Instructor model and instructors endpoints

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,9 +6,15 @@ from accounts.models import Instructor
 User = get_user_model()
 
 
-class InstructorAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',)
+class InstructorInterviewAdmin(admin.ModelAdmin):
+    fields = ('user', 'display_name', 'interviewed', )
+    list_display = ('pk', 'user', 'display_name', )
+    list_filter = ('interviewed', )
+    list_select_related = ('user', )
+    search_fields = ['user__email', 'display_name', ]
+    readonly_fields = ('user', 'display_name', )
+    ordering = ('pk', )
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(Instructor, InstructorInterviewAdmin)
