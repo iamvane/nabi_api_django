27eb27c5bfeed9a599f19f7251fc388e31f2ab73
Luis Llave <llaveluis@gmail.com>
2019-12-05 18:47:05 -0500

    [NM-136] Add test for request-references endpoint

--- references/tests/test_references_api.py
+++ references/tests/test_references_api.py
@@ -1,11 +1,32 @@
 """Tests for references API"""
-import operator
-
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 from rest_framework import status
 
 from accounts.tests.base_test_class import BaseTest
+
+User = get_user_model()
+
+
+class InstructorRegisterReferencesTest(BaseTest):
+    fixtures = ['01_core_users.json', ]
+    login_data = {
+        'email': 'luisinstruct@yopmail.com',
+        'password': 'T3st11ng'
+    }
+
+    def setUp(self):
+        super().setUp()
+        self.url = '{}/v1/request-references/'.format(settings.HOSTNAME_PROTOCOL)
+
+    def test_register_references(self):
+        """Test registration of references"""
+        user = User.objects.get(email=self.login_data['email'])
+        self.assertEqual(user.reference_requests.count(), 0)
+        response = self.client.post(self.url, data={'emails': ["luistest003@yopmail.com", "luistest005@yopmail.com"]})
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        self.assertEqual(user.reference_requests.count(), 2)
 
 
 class InstructorReferencesListTest(BaseTest):
