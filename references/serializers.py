bc97c6cc6768d55ec6d567371443c7ac60e10451
Luis Llave <llaveluis@gmail.com>
2019-12-06 14:09:07 -0500

    [NM-136] Add restriction to avoid repeated emails for user

--- references/serializers.py
+++ references/serializers.py
@@ -10,3 +10,13 @@ class RegisterRequestReferenceSerializer(serializers.Serializer):
         for email in validated_data['emails']:
             ReferenceRequest.objects.create(email=email, user=self.context['user'])
         return {}
+
+    def validate(self, data):
+        data = super().validate(data)
+        new_list = []
+        for email in data['emails']:
+            if not ReferenceRequest.objects.filter(email=email, user=self.context['user']).exists():
+                new_list.append(email)
+        if not new_list:
+            raise serializers.ValidationError('Not valid emails were provided')
+        return {'emails': new_list}
