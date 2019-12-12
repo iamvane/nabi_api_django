462363905028f189b2efade3181315e79f566bc8
Luis Llave <llaveluis@gmail.com>
2019-12-12 09:29:30 -0500

    [NM-133] Fix bug about to update completed field when related data is changed

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 
 from core.constants import ROLE_INSTRUCTOR
 
-from .models import Instructor, get_account
+from .models import Availability, Education, Employment, Instructor, InstructorInstruments, InstructorLessonRate, \
+    PhoneNumber, get_account
 
 User = get_user_model()
 
@@ -24,6 +25,12 @@ def set_display_name(sender, instance, **kwargs):
         account.set_display_name()
 
 
+@receiver(post_save, sender=Availability)
+@receiver(post_save, sender=Education)
+@receiver(post_save, sender=Employment)
+@receiver(post_save, sender=InstructorInstruments)
+@receiver(post_save, sender=InstructorLessonRate)
+@receiver(post_save, sender=PhoneNumber)
 @receiver(post_save, sender=Instructor)
 @receiver(post_save, sender=User)
 def change_completed_profile(sender, instance, **kwargs):
@@ -33,4 +40,10 @@ def change_completed_profile(sender, instance, **kwargs):
     if isinstance(instance, Instructor):
         instance.update_completed()
     if isinstance(instance, User) and instance.get_role() == ROLE_INSTRUCTOR:
+        instance.instructor.update_completed()
+    if isinstance(instance, PhoneNumber) and instance.user.get_role == ROLE_INSTRUCTOR:
+        instance.user.instructor.update_completed()
+    if isinstance(instance, InstructorInstruments) or isinstance(instance, InstructorLessonRate) \
+            or isinstance(instance, Availability) or isinstance(instance, Education) \
+            or isinstance(instance, Employment):
         instance.instructor.update_completed()
