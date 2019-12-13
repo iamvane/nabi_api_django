97ca6779371e2168e874782fa94a2c4dc7e1f78d
Luis Llave <llaveluis@gmail.com>
2019-12-13 07:53:30 -0500

    [NM-133] Improve tests for instructors API endpoint

--- accounts/tests/test_instructors_api.py
+++ accounts/tests/test_instructors_api.py
@@ -8,7 +8,8 @@ from rest_framework import status
 from .base_test_class import BaseTest
 
 
-class InstructorsTest(BaseTest):
+class InstructorsListTest(BaseTest):
+    """Tests for instructors list"""
     fixtures = ['01_core_users.json', '02_accounts_instructors.json', '05_lesson_instruments.json',
                 '06_accounts_availabilities.json', '07_accounts_educations.json', '08_accounts_employments.json',
                 '09_accounts_instructoradditionalqualifications.json', '10_accounts_instructoragegroups.json',
@@ -52,8 +53,79 @@ class InstructorsTest(BaseTest):
         self.assertListEqual(list_response, self.current_data)
 
 
-class ItemInstructorsTest(BaseTest):
-    fixtures = ['01_core_users.json', '02_accounts_instructors.json', '05_lesson_instruments.json']
+class InstructorsListNoAuthTest(BaseTest):
+    """Tests for instructors list, without authenticated user"""
+    fixtures = ['01_core_users.json', '02_accounts_instructors.json', '05_lesson_instruments.json',
+                '06_accounts_availabilities.json', '07_accounts_educations.json', '08_accounts_employments.json',
+                '09_accounts_instructoradditionalqualifications.json', '10_accounts_instructoragegroups.json',
+                '11_accounts_instructorinstruments.json', '12_accounts_instructorlessonrates.json',
+                '14_accounts_instructorplaceforlessons.json', '18_phonenumbers.json']
+
+    def setUp(self):
+        self.url = '{}/v1/instructors/'.format(settings.HOSTNAME_PROTOCOL)
+
+    def test_get_data_filters(self):
+        """Test getting data using various filters (GET parameters)"""
+        response = self.client.get(self.url + '?minRate=11.0')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?maxRate=11.0')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?gender=male')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?availability=mon8to10')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?instruments=flute')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?instruments=flute,piano')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 2)
+        self.assertEqual(len(data['results']), 2)
+        response = self.client.get(self.url + '?studentAges=adults')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?placeForLessons=online')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?qualifications=ear_training')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
+        response = self.client.get(self.url + '?gender=male&max_rate=10')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 0)
+        self.assertEqual(len(data['results']), 0)
+        response = self.client.get(self.url + '?availability=mon8to10,thu6to9')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        data = response.json()
+        self.assertEqual(data['count'], 2)
+        self.assertEqual(len(data['results']), 2)
+
+
+class DetailInstructorBasicTest(BaseTest):
+    """Test for instructor's detail API endpoint"""
+    fixtures = ['01_core_users.json', '02_accounts_instructors.json', '05_lesson_instruments.json', ]
     login_data = {
         'email': 'luisinstruct@yopmail.com',
         'password': 'T3st11ng'
@@ -84,3 +156,50 @@ class ItemInstructorsTest(BaseTest):
         """Test getting data of a non-existent entry"""
         response = self.client.get(self.url + '33/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, msg=response.content.decode())
+
+
+class DetailInstructorTest(BaseTest):
+    """Test for getting instructor's details, almost all data"""
+    fixtures = ['01_core_users.json', '02_accounts_instructors.json', '06_accounts_availabilities.json',
+                '09_accounts_instructoradditionalqualifications.json', '10_accounts_instructoragegroups.json',
+                '12_accounts_instructorlessonrates.json', '13_accounts_instructorlessonsizes.json',
+                '14_accounts_instructorplaceforlessons.json', '18_phonenumbers.json']
+    login_data = {
+        'email': 'luisinstruct@yopmail.com',
+        'password': 'T3st11ng'
+    }
+    current_data = {"id": 1, 'userId': 5, 'displayName': 'Luis I.', 'age': 44, 'interviewed': False,
+                    'avatar': None, 'memberSince': '2019', 'yearsOfExperience': 0, 'reviews': 0,
+                    "bioTitle": "Music instructor", 'languages': None,
+                    "bioDescription": "I'm a professional music instructor", 'instruments': [],
+                    'education': [], 'employment': [],
+                    'availability': {"mon8to10": True, "mon10to12": False, "mon12to3": True, "mon3to6": False,
+                                     "mon6to9": False, "tue8to10": False, "tue10to12": False, "tue12to3": False,
+                                     "tue3to6": False, "tue6to9": False, "wed8to10": False, "wed10to12": False,
+                                     "wed12to3": False, "wed3to6": False, "wed6to9": False, "thu8to10": False,
+                                     "thu10to12": False, "thu12to3": False, "thu3to6": False, "thu6to9": False,
+                                     "fri8to10": False, "fri10to12": False, "fri12to3": False, "fri3to6": False,
+                                     "fri6to9": False, "sat8to10": False, "sat10to12": False, "sat12to3": False,
+                                     "sat3to6": False, "sat6to9": False, "sun8to10": False, "sun10to12": False,
+                                     "sun12to3": False, "sun3to6": False, "sun6to9": False},
+                    'ageGroup': {"children": True, "teens": False, "adults": True, "seniors": True},
+                    'lessonSize': {"oneStudent": True, "smallGroups": True, "largeGroups": False},
+                    'lessonsTaught': 0, 'music': ['piano', 'guitar'],
+                    'placeForLessons': {"home": True, "studio": False, "online": True},
+                    'qualifications': {"certifiedTeacher": True, "musicTherapy": False, "musicProduction": False,
+                                       "earTraining": True, "conducting": False, "virtuosoRecognition": False,
+                                       "performance": False, "musicTheory": True, "youngChildrenExperience": False,
+                                       "repertoireSelection": False},
+                    'rates': {'mins30': '10.00', 'mins45': '15.00', 'mins60': '20.00', 'mins90': '30.00'},
+                    'studioAddress': None, 'travelDistance': None}
+
+    def setUp(self):
+        super().setUp()
+        self.url = '{}/v1/instructors/'.format(settings.HOSTNAME_PROTOCOL)
+
+    def test_get_single_data(self):
+        """Test getting almost all data of a single entry"""
+        response = self.client.get(self.url + str(self.current_data['id']) + '/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content.decode())
+        dict_data = response.json()
+        self.assertDictEqual(dict_data, self.current_data)
