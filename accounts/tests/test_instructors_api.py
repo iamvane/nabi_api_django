06b5498f5b69ea23efae5a87e2252a322e1845e0
Luis Llave <llaveluis@gmail.com>
2019-12-12 20:42:25 -0500

    [NM-133] Update test for instructors API endpoint

--- accounts/tests/test_instructors_api.py
+++ accounts/tests/test_instructors_api.py
@@ -11,17 +11,33 @@ from .base_test_class import BaseTest
 class InstructorsTest(BaseTest):
     fixtures = ['01_core_users.json', '02_accounts_instructors.json', '05_lesson_instruments.json',
                 '06_accounts_availabilities.json', '07_accounts_educations.json', '08_accounts_employments.json',
+                '09_accounts_instructoradditionalqualifications.json', '10_accounts_instructoragegroups.json',
                 '11_accounts_instructorinstruments.json', '12_accounts_instructorlessonrates.json',
-                '18_phonenumbers.json']
+                '14_accounts_instructorplaceforlessons.json', '18_phonenumbers.json']
     login_data = {
         'email': 'luisinstruct@yopmail.com',
         'password': 'T3st11ng'
     }
-    current_data = [{"id": 1, "displayName": "Luis I.", "age": 44, "avatar": None, "interviewed": False,
+    current_data = [{"id": 1, "displayName": "Luis I.", "age": 44, "avatar": None, "interviewed": False, "gender": None,
                      "bioTitle": "Music instructor", "bioDescription": "I'm a professional music instructor",
                      "location": "Houston, TX", "reviews": 0, "lessonsTaught": 0, "instruments": ['guitar', 'piano'],
                      "rates": {'mins30': '10.00', 'mins45': '15.00', 'mins60': '20.00', 'mins90': '30.00'},
-                     "lastLogin": "2019-10-30 17:13:11", "memberSince": "2019"}]
+                     "qualifications": {"certifiedTeacher": True, "musicTherapy": False, "musicProduction": False,
+                                        "earTraining": True, "conducting": False, "virtuosoRecognition": False,
+                                        "performance": False, "musicTheory": True, "youngChildrenExperience": False,
+                                        "repertoireSelection": False},
+                     "placeForLessons": {"home": True, "studio": False, "online": True},
+                     "availability": {"mon8to10": True, "mon10to12": False, "mon12to3": True, "mon3to6": False,
+                                      "mon6to9": False, "tue8to10": False, "tue10to12": False, "tue12to3": False,
+                                      "tue3to6": False, "tue6to9": False, "wed8to10": False, "wed10to12": False,
+                                      "wed12to3": False, "wed3to6": False, "wed6to9": False, "thu8to10": False,
+                                      "thu10to12": False, "thu12to3": False, "thu3to6": False, "thu6to9": False,
+                                      "fri8to10": False, "fri10to12": False, "fri12to3": False, "fri3to6": False,
+                                      "fri6to9": False, "sat8to10": False, "sat10to12": False, "sat12to3": False,
+                                      "sat3to6": False, "sat6to9": False, "sun8to10": False, "sun10to12": False,
+                                      "sun12to3": False, "sun3to6": False, "sun6to9": False},
+                     "student_ages": {"children": True, "teens": False, "adults": True, "seniors": True},
+                     "languages": None, "lastLogin": "2019-10-30 17:13:11", "memberSince": "2019"}]
 
     def setUp(self):
         super().setUp()
