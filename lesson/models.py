8d7884b805da95d7da2ad2bb57d6a90ace8cd7b5
Luis Llave <llaveluis@gmail.com>
2019-12-18 09:24:25 -0500

    [NM-87] Change status definition place to constants file

--- lesson/models.py
+++ lesson/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 from accounts.models import TiedStudent
-from core.constants import SKILL_LEVEL_CHOICES, PLACE_FOR_LESSONS_CHOICES, LESSON_DURATION_CHOICES
+from core.constants import (LESSON_DURATION_CHOICES, LR_NO_SEEN, LR_STATUSES, PLACE_FOR_LESSONS_CHOICES,
+                            SKILL_LEVEL_CHOICES, )
 
 User = get_user_model()
 
@@ -20,12 +21,6 @@ class Instrument(models.Model):
 
 class LessonRequest(models.Model):
     # user making the request, can be Parent or Student
-    SEEN_STATUS = 'seen'
-    NO_SEEN_STATUS = 'no seen'
-    STATUSES = (
-        (SEEN_STATUS, SEEN_STATUS),
-        (NO_SEEN_STATUS, NO_SEEN_STATUS),
-    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lesson_requests')
     title = models.CharField(max_length=100)
     message = models.TextField()
@@ -34,7 +29,7 @@ class LessonRequest(models.Model):
     place_for_lessons = models.CharField(max_length=100, choices=PLACE_FOR_LESSONS_CHOICES)
     lessons_duration = models.CharField(max_length=100, choices=LESSON_DURATION_CHOICES)
     students = models.ManyToManyField(TiedStudent)
-    status = models.CharField(max_length=100, choices=STATUSES, blank=True, default=NO_SEEN_STATUS)
+    status = models.CharField(max_length=100, choices=LR_STATUSES, blank=True, default=LR_NO_SEEN)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
