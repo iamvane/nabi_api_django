5f64fa6a589b98a6ca699ad8d6830dafc530bd37
Luis Llave <llaveluis@gmail.com>
2020-01-01 11:33:25 -0500

    [NM-94] Fix implementation of get_location method

--- accounts/models.py
+++ accounts/models.py
@@ -54,12 +54,13 @@ class IUserAccount(models.Model):
             :type result_type: 'string' (to return 'city, state' string), 'tuple' (to return country, state, city)"""
         if self.coordinates:
             try:
-                lat = self.coordinates.coords[0]
-                lng = self.coordinates.coords[1]
+                lat = self.coordinates.coords[1]
+                lng = self.coordinates.coords[0]
             except Exception:
-                return ''
-            if lat < -90 or lat > 90 or lng < -180 or lng > 180:
-                return ''
+                if result_type == 'string':
+                    return ''
+                else:
+                    return ()
             try:
                 geocoder = Geocoder(api_key=settings.GOOGLE_MAPS_API_KEY)
                 locations = geocoder.reverse_geocode(lat, lng)
