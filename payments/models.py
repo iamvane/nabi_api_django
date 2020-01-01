07effec1b05321612a5341be542619e5f1710144
Luis Llave <llaveluis@gmail.com>
2020-01-01 18:34:04 -0500

    [NM-94] Improve Payment model, to handle relation with a service

--- payments/models.py
+++ payments/models.py
@@ -1,12 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from core.constants import *
+
 User = get_user_model()
 
 
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
+    service = models.CharField(max_length=100, choices=SERVICES_CHOICES)
+    service_id = models.IntegerField(blank=True, null=True)
     amount = models.DecimalField(max_digits=9, decimal_places=4)
     description = models.CharField(max_length=300)
     charge_id = models.CharField(max_length=300)
+    status = models.CharField(max_length=100, choices=PY_STATUSES, default=PY_REGISTERED)
     payment_date = models.DateTimeField(auto_now_add=True)
