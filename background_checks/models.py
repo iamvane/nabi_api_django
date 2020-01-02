a03c239f255db3d578bb36eee2cc2a23b1b53424
Luis Llave <llaveluis@gmail.com>
2020-01-02 12:28:21 -0500

    [NM-94] Change BackgroundCheckRequest model, to include relevant data

--- background_checks/models.py
+++ background_checks/models.py
@@ -1,6 +1,7 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+from accounts.models import Instructor
 from core.models import ProviderRequest, User
 
 
@@ -16,12 +17,14 @@ class BackgroundCheckRequest(models.Model):
         (CANCELLED, CANCELLED),
     )
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='bg_check_requests')
-    status = models.CharField(max_length=100, choices=STATUSES)
+    instructor = models.ForeignKey(Instructor, null=True, on_delete=models.SET_NULL, related_name='bg_check_requests')
+    status = models.CharField(max_length=100, choices=STATUSES, default=PRELIMINARY)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class BackgroundCheckStep(models.Model):
+    """Register steps made with Provider"""
     request = models.ForeignKey(BackgroundCheckRequest, on_delete=models.CASCADE, related_name='steps')
     step = models.CharField(max_length=50)
     resource_id = models.CharField(max_length=200)   # id of resource, returned by provider
