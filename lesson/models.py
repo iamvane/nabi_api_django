b690cc6a783c0784338259f5ddc0fda87620449c
Luis Llave <llaveluis@gmail.com>
2020-01-08 14:21:32 -0500

    [NM-114] Update LessonRequest model and add LessonBooking model

--- lesson/models.py
+++ lesson/models.py
@@ -2,15 +2,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 from accounts.models import Instructor, Parent, Student, TiedStudent
-from core.constants import (LESSON_DURATION_CHOICES, LR_NO_SEEN, LR_STATUSES, PLACE_FOR_LESSONS_CHOICES,
-                            SKILL_LEVEL_CHOICES, )
-
+from core.constants import *
 from payments.models import Payment
 
 User = get_user_model()
 
-
-# TODO: classes Lesson, Application
 
 class Instrument(models.Model):
     name = models.CharField(max_length=250)
@@ -33,7 +29,8 @@ class LessonRequest(models.Model):
     travel_distance = models.IntegerField(blank=True, null=True)
 
     students = models.ManyToManyField(TiedStudent)
-    status = models.CharField(max_length=100, choices=LR_STATUSES, blank=True, default=LR_NO_SEEN)
+    status = models.CharField(max_length=100, choices=LESSON_REQUEST_STATUSES,
+                              blank=True, default=LESSON_REQUEST_ACTIVE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -58,29 +55,25 @@ class Application(models.Model):
     instructor = models.ForeignKey(Instructor, related_name='applications', on_delete=models.CASCADE)
     rate = models.DecimalField(max_digits=9, decimal_places=4)
     message = models.TextField()
-    status = models.CharField(max_length=100, choices=LR_STATUSES, default=LR_NO_SEEN)
+    status = models.CharField(max_length=100, choices=APPLICATION_STATUSES, default=APPLICATION_NO_SEEN)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class LessonBooking(models.Model):
     REQUESTED = 'requested'
-    PAID = 'paid'
     CANCELLED = 'cancelled'
     STATUSES = (
         (REQUESTED, REQUESTED),
-        (PAID, PAID),
+        (CANCELLED, CANCELLED),
     )
-    student = models.ForeignKey(Student, blank=True, null=True, on_delete=models.CASCADE,
-                                related_name='lesson_bookings')
-    parent = models.ForeignKey(Parent, blank=True, null=True, on_delete=models.CASCADE,
-                               related_name='lesson_bookings')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='lesson_bookings')
     quantity = models.IntegerField()
     total_amount = models.DecimalField(max_digits=9, decimal_places=4)
-    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='lesson_bookings')
-    lesson_rate = models.DecimalField(max_digits=9, decimal_places=4)
+    request = models.ForeignKey(LessonRequest, on_delete=models.CASCADE, related_name='booking')
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='booking')
     status = models.CharField(max_length=50, choices=STATUSES, default=REQUESTED)
     payment = models.ForeignKey(Payment, blank=True, null=True, on_delete=models.CASCADE,
                                 related_name='lesson_bookings')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
