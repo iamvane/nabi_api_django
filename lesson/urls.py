9fefe9219921dc3775cc291ba082a9f9a0a99d37
Luis Llave <llaveluis@gmail.com>
2020-01-08 19:24:32 -0500

    [NM-167] Add endpoint to get a list of applications belong to a lesson request

--- lesson/urls.py
+++ lesson/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('applications/', views.ApplicationView.as_view()),
     path('lesson-request-list/', views.LessonRequestListView.as_view()),
     path('lesson-request-item/<int:pk>/', views.LessonRequestItemListView.as_view()),
+    path('lesson-request-list/', views.LessonRequestList.as_view()),
+    path('application-list/<int:lesson_req_id>/', views.ApplicationListView.as_view()),
 ]
