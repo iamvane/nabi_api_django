89ac808295795e54cf029e119160aec7a94766be
Luis Llave <llaveluis@gmail.com>
2020-01-13 16:41:35 -0500

    [NM-115] Adjust admin for Instructor model, to enable filtering by location and distance

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,14 @@
+from pygeocoder import Geocoder, GeocoderError
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.gis.db.models import PointField
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+from django.db.models.functions import Cast
 
 from accounts.models import (Education, Employment, Instructor, InstructorAdditionalQualifications,
                              InstructorAgeGroup, InstructorInstruments, InstructorLessonRate, InstructorLessonSize)
@@ -43,18 +51,64 @@ class LessonSizeAdmin(admin.TabularInline):
     extra = 1
 
 
-class InstructorInterviewAdmin(admin.ModelAdmin):
-    fields = ('user', 'display_name', 'age', 'avatar', 'bio_title', 'bio_description',
-              'music', 'interviewed', 'languages', 'studio_address', 'travel_distance', 'experience_years')
-    list_display = ('pk', 'user', 'display_name', )
-    list_filter = ('interviewed', )
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ('user', 'display_name', 'age', 'avatar', 'bio_title', 'bio_description', 'distance',
+              'music', 'interviewed', 'languages', 'studio_address', 'travel_distance', 'experience_years', )
+    list_display = ('pk', 'user', 'display_name', 'distance', )
+    list_filter = ('interviewed', 'completed', )
     list_select_related = ('user', )
-    search_fields = ['user__email', 'display_name', ]
-    readonly_fields = ('user', 'display_name', 'age', 'experience_years')
+    search_fields = ('user__email', 'display_name', )
+    location_search_values = {}
+    readonly_fields = ('user', 'display_name', 'age', 'experience_years', 'distance', )
     ordering = ('pk', )
     inlines = [EducationInline, EmploymentInline, AdicionalQualificationsAdmin, AgeGroupAdmin,
                InstrumentsAdmin, LessonRateAdmin, LessonSizeAdmin]
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(distance=Cast(None, PointField()))
+
+    def changelist_view(self, request, extra_context=None):
+        self.location_search_values = {}
+        request.GET._mutable = True
+        keys = dict.fromkeys(request.GET, 1)
+        if keys.get('address'):
+            if len(request.GET.get('address')) > 0 and request.GET.get('address')[0]:
+                self.location_search_values['address'] = request.GET.pop('address')[0]
+            else:
+                request.GET.pop('address')
+            if keys.get('distance'):
+                if len(request.GET.get('distance')) > 0 and request.GET.get('distance')[0]:
+                    self.location_search_values['distance'] = request.GET.pop('distance')[0]
+                else:
+                    request.GET.pop('distance')
+            if self.location_search_values.get('address') is not None and self.location_search_values.get('distance') is None:
+                self.location_search_values['distance'] = 50
+        request.GET._mutable = False
+        return super().changelist_view(request, extra_context={'location_values': self.location_search_values})
+
+    def get_search_results(self, request, queryset, search_term):
+        if self.location_search_values:
+            geocoder = Geocoder(api_key=settings.GOOGLE_MAPS_API_KEY)
+            try:
+                results = geocoder.geocode(self.location_search_values['address'])
+            except GeocoderError as e:
+                self.location_search_values = {}
+                raise Exception(e.status, e.response)
+            point = Point(results[0].coordinates[1], results[0].coordinates[0], srid=4326)
+            qs = queryset.filter(coordinates__isnull=False).annotate(
+                distance=Distance('coordinates', point)
+            ).filter(distance__lte=D(mi=self.location_search_values['distance']))
+        else:
+            qs = queryset
+        return super().get_search_results(request, qs, search_term)
+
+    def distance(self, instance):
+        if instance.distance:
+            return instance.distance.mi
+        else:
+            return None
+
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Instructor, InstructorInterviewAdmin)
+admin.site.register(Instructor, InstructorAdmin)
---
+++ accounts/templatetags/for_admin.py
@@ -0,0 +1,23 @@
+from django.template import Library
+from django.contrib.admin.views.main import SEARCH_VAR
+from django.contrib.admin.templatetags.base import InclusionAdminNode
+from django.contrib.admin.templatetags.admin_list import search_form
+
+register = Library()
+
+
+def search_form(location, cl):
+    """
+    Display a search form for searching the list.
+    """
+    return {
+        'cl': cl,
+        'show_result_count': cl.result_count != cl.full_result_count,
+        'search_var': SEARCH_VAR,
+        'location_values': location
+    }
+
+
+@register.tag(name='search_form')
+def search_form_tag(parser, token):
+    return InclusionAdminNode(parser, token, func=search_form, template_name='search_form.html', takes_context=False)
