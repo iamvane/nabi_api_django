96224251f91af9a25c5b9a67a77622ef48610a84
iamvane <vane.charles24@gmail.com>
2020-01-20 09:29:37 -0500

    [NO-TICKET] added contact lists ids (#111)
    * [NO-TICKET] added contact lists ids
    
    * [NO-TICKET] added missing character

--- nabi_api_django/production_settings/__init__.py
+++ nabi_api_django/production_settings/__init__.py
@@ -233,3 +233,9 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
+
+SENDGRID_CONTACT_LIST_IDS = {
+    'instructors': os.environ['SENDGRID_CONTACT_LIST_INSTRUCTORS'],
+    'parents': os.environ['SENDGRID_CONTACT_LIST_PARENTS'],
+    'students': os.environ['SENDGRID_CONTACT_LIST_STUDENTS']
+}
