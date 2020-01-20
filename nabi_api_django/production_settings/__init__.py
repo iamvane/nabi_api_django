ae7490447b0134844bf1b06a1095f7a8cd323c5b
iamvane <vane.charles24@gmail.com>
2020-01-20 09:09:32 -0500

    [NO-TICKET] added sendgrid constant

--- nabi_api_django/production_settings/__init__.py
+++ nabi_api_django/production_settings/__init__.py
@@ -122,6 +122,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 GOOGLE_FORM_REFERENCES_URL = 'https://forms.gle/MuGhfwUARTW9uzrU9'
+SENDGRID_API_BASE_URL = 'https://api.sendgrid.com/v3/'
 
 REST_PAGE_SIZE = 20
 
