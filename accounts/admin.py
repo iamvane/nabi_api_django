44824c83d4b84c4dbc214aba782d0ec6948c233d
Luis <llaveluis@gmail.com>
2020-01-21 12:18:54 -0500

    [NM-115] Customize admin interface for Instructor model and app lesson's models
    * [NM-115] Add fields in instructor view (admin interface)
    
    * [NM-115] Adjust admin for Instructor model, to enable filtering by location and distance
    
    * [NM-115] Configure admin interface for Application, LessonBooking and LessonRequest models
    
    * [NM-115] Add search instructor by instrument and place_for_lesson. Add minimal improvements
    
    * [NM-115] Add location field to admin interface, and update coordinates if new location value is provided

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,156 @@
+from pygeocoder import Geocoder, GeocoderError
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import Instructor
+from django.contrib.gis.db.models import PointField
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+from django.db.models import Q
+from django.db.models.functions import Cast
+
+from accounts.models import (Education, Employment, Instructor, InstructorAdditionalQualifications,
+                             InstructorAgeGroup, InstructorInstruments, InstructorLessonRate, InstructorLessonSize)
 
 User = get_user_model()
 
 
-class InstructorInterviewAdmin(admin.ModelAdmin):
-    fields = ('user', 'display_name', 'interviewed', )
-    list_display = ('pk', 'user', 'display_name', )
-    list_filter = ('interviewed', )
+class EducationInline(admin.TabularInline):
+    model = Education
+    extra = 1
+
+
+class EmploymentInline(admin.TabularInline):
+    model = Employment
+    extra = 1
+
+
+class AdicionalQualificationsAdmin(admin.TabularInline):
+    model = InstructorAdditionalQualifications
+    extra = 1
+
+
+class AgeGroupAdmin(admin.TabularInline):
+    model = InstructorAgeGroup
+    extra = 1
+
+
+class InstrumentsAdmin(admin.TabularInline):
+    model = InstructorInstruments
+    extra = 1
+
+
+class LessonRateAdmin(admin.TabularInline):
+    model = InstructorLessonRate
+    extra = 1
+
+
+class LessonSizeAdmin(admin.TabularInline):
+    model = InstructorLessonSize
+    extra = 1
+
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ('user', 'display_name', 'age', 'avatar', 'bio_title', 'bio_description', 'distance', 'location',
+              'music', 'interviewed', 'languages', 'studio_address', 'travel_distance', 'experience_years', )
+    list_display = ('pk', 'user', 'display_name', 'distance', )
+    list_filter = ('interviewed', 'complete', )
     list_select_related = ('user', )
-    search_fields = ['user__email', 'display_name', ]
-    readonly_fields = ('user', 'display_name', )
+    search_fields = ('user__email', 'display_name', 'instruments__name')
+    location_search_values = {}
+    places_search_values = {}
+    readonly_fields = ('user', 'display_name', 'age', 'experience_years', 'distance', )
     ordering = ('pk', )
+    inlines = [EducationInline, EmploymentInline, AdicionalQualificationsAdmin, AgeGroupAdmin,
+               InstrumentsAdmin, LessonRateAdmin, LessonSizeAdmin]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(distance=Cast(None, PointField()))
+
+    def changelist_view(self, request, extra_context=None):
+        self.location_search_values = {}
+        self.places_search_values = {}
+        request.GET._mutable = True
+        keys = dict.fromkeys(request.GET, 1)
+        if keys.get('address'):
+            if len(request.GET.get('address')) > 0 and request.GET.get('address')[0]:
+                self.location_search_values['address'] = request.GET.pop('address')[0]
+            else:
+                request.GET.pop('address')
+            if keys.get('distance'):
+                if len(request.GET.get('distance')) > 0 and request.GET.get('distance')[0] is not None:
+                    self.location_search_values['distance'] = request.GET.pop('distance')[0]
+                else:
+                    request.GET.pop('distance')
+            if self.location_search_values.get('address') is not None and self.location_search_values.get('distance') is None:
+                self.location_search_values['distance'] = 50
+        if keys.get('home'):
+            self.places_search_values['home'] = True
+            request.GET.pop('home')
+        if keys.get('online'):
+            self.places_search_values['online'] = True
+            request.GET.pop('online')
+        if keys.get('studio'):
+            self.places_search_values['studio'] = True
+            request.GET.pop('studio')
+        request.GET._mutable = False
+        return super().changelist_view(request, extra_context={'location_values': self.location_search_values,
+                                                               'places_values': self.places_search_values})
+
+    def get_search_results(self, request, queryset, search_term):
+        if self.location_search_values:
+            geocoder = Geocoder(api_key=settings.GOOGLE_MAPS_API_KEY)
+            try:
+                results = geocoder.geocode(self.location_search_values['address'])
+            except GeocoderError as e:
+                self.location_search_values = {}
+                raise Exception(e.status, e.response)
+            point = Point(results[0].coordinates[1], results[0].coordinates[0], srid=4326)
+            qs = queryset.filter(coordinates__isnull=False).annotate(
+                distance=Distance('coordinates', point)
+            ).filter(distance__lte=D(mi=self.location_search_values['distance']))
+        else:
+            qs = queryset
+        if self.places_search_values:
+            condics = None
+            if self.places_search_values.get('home'):
+                if condics is None:
+                    condics = Q(instructorplaceforlessons__home=True)
+                else:
+                    condics = condics | Q(instructorplaceforlessons__home=True)
+            if self.places_search_values.get('online'):
+                if condics is None:
+                    condics = Q(instructorplaceforlessons__online=True)
+                else:
+                    condics = condics | Q(instructorplaceforlessons__online=True)
+            if self.places_search_values.get('studio'):
+                if condics is None:
+                    condics = Q(instructorplaceforlessons__studio=True)
+                else:
+                    condics = condics | Q(instructorplaceforlessons__studio=True)
+            if condics:
+                qs = qs.filter(condics)
+        return super().get_search_results(request, qs, search_term)
+
+    def distance(self, instance):
+        if instance.distance is not None:
+            return instance.distance.mi
+        else:
+            return None
+
+    def save_model(self, request, obj, form, change):
+        if 'location' in form.changed_data:
+            geocoder = Geocoder(api_key=settings.GOOGLE_MAPS_API_KEY)
+            try:
+                results = geocoder.geocode(obj.location)
+            except GeocoderError as e:
+                raise Exception(e.status, e.response)
+            obj.coordinates = Point(results[0].coordinates[1], results[0].coordinates[0])
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Instructor, InstructorInterviewAdmin)
+admin.site.register(Instructor, InstructorAdmin)
---
+++ accounts/templatetags/for_admin.py
@@ -0,0 +1,24 @@
+from django.template import Library
+from django.contrib.admin.views.main import SEARCH_VAR
+from django.contrib.admin.templatetags.base import InclusionAdminNode
+from django.contrib.admin.templatetags.admin_list import search_form
+
+register = Library()
+
+
+def search_form(location, places, cl):
+    """
+    Display a search form for searching the list.
+    """
+    return {
+        'cl': cl,
+        'show_result_count': cl.result_count != cl.full_result_count,
+        'search_var': SEARCH_VAR,
+        'location_values': location,
+        'places_values': places
+    }
+
+
+@register.tag(name='search_form')
+def search_form_tag(parser, token):
+    return InclusionAdminNode(parser, token, func=search_form, template_name='search_form.html', takes_context=False)
