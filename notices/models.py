e9298290db38a2c6b7fa7b7e0e5366879341437a
Luis Llave <llaveluis@gmail.com>
2020-01-27 15:07:59 -0500

    [NM-210] Add fields to Offer model

--- notices/models.py
+++ notices/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Offer(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
+    percent_discount = models.IntegerField(blank=True, null=True)
+    free_lesson = models.BooleanField(default=False)
     show_at = models.DateTimeField(blank=True, null=True)
     hide_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
