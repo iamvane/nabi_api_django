f4c58939112828c74c1b33143a1df1212256973c
Luis <llaveluis@gmail.com>
2020-01-27 09:37:04 -0500

    [NM-139] Add task to alert instructors about a new lesson request near him/her
    * Update requirements including packages for celery usage
    
    * [NM-139] Add model for log called tasks
    
    * [NM-139] Add task for send email about new request near of instructor
    
    * [NM-139] Add URL parameter for rendering email template
    
    * [NM-139] Make some changes to template content

--- lesson/urls.py
+++ lesson/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('lesson-request/', views.LessonRequestView.as_view()),
+    path('lesson-request/', views.LessonRequestView.as_view(), name='lesson_request'),
     path('lesson-request/<int:pk>/', views.LessonRequestItemView.as_view()),
     path('applications/', views.ApplicationView.as_view()),
     path('lesson-request-list/', views.LessonRequestListView.as_view()),
-    path('lesson-request-item/<int:pk>/', views.LessonRequestItemListView.as_view()),
+    path('lesson-request-item/<int:pk>/', views.LessonRequestItemListView.as_view(), name='lesson_request_item'),
     path('application-list/<int:lesson_req_id>/', views.ApplicationListView.as_view()),
     path('booking-lessons/', views.LessonBookingRegisterView.as_view()),
     path('application-data/<int:app_id>/', views.ApplicationDataView.as_view()),
--- nabi_api_django/settings.py
+++ nabi_api_django/settings.py
@@ -109,6 +109,10 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
 
+CELERY_BROKER_URL = os.environ.get('BROKER_URL', '')
+BROKER_POOL_LIMIT = 1
+
+
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/New_York'
