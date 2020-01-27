356f50ff5b397446d4cf37500bc6c5332117ee61
Luis Llave <llaveluis@gmail.com>
2020-01-27 09:31:26 -0500

    [NM-139] Make some changes to template content

--- lesson/utils.py
+++ lesson/utils.py
@@ -9,13 +9,11 @@ def send_alert_request_instructor(instructor, lesson_request, requestor_account)
     from_email = 'Nabi Music <' + settings.DEFAULT_FROM_EMAIL + '>'
     template = 'request_advice_email.html'
     plain_template = 'request_advice_email_plain.html'
-    subject = 'There is a New Request Near You'
+    subject = 'There is a New Lesson Request Near You'
     location_tuple = requestor_account.get_location(result_type='tuple')
     params = {
         'request_title': lesson_request.title,
         'location': '{}, {}'.format(location_tuple[2], location_tuple[1]),
-        'reference_url': '{}{}'.format(settings.HOSTNAME_PROTOCOL, reverse_lazy('lesson_request_item',
-                                                                                kwargs={'pk': lesson_request.id})
-                                       )
+        'reference_url': '{}/request/{}'.format(settings.HOSTNAME_PROTOCOL, lesson_request.id)
     }
     send_email(from_email, [instructor.user.email], subject, template, plain_template, params)
