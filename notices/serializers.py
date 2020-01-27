7ac89890a8a292a82860b47e96746b287e5e027e
Luis Llave <llaveluis@gmail.com>
2020-01-27 15:08:20 -0500

    [NM-210] Improve API for return values about free lesson and percent of discount

--- notices/serializers.py
+++ notices/serializers.py
@@ -4,7 +4,11 @@ from .models import Offer
 
 
 class OfferDetailSerializer(serializers.ModelSerializer):
+    freeLesson = serializers.BooleanField(source='free_lesson')
+    hideAt = serializers.DateTimeField(source='hide_at', format='%Y-%m-%d %H:%M:%S')
+    percentDiscount = serializers.IntegerField(source='percent_discount')
+    showAt = serializers.DateTimeField(source='show_at', format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
         model = Offer
-        fields = ['name', 'content', 'show_at', 'hide_at']
+        fields = ['content', 'freeLesson', 'hideAt',  'name', 'percentDiscount', 'showAt']
