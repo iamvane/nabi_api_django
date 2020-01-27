c0a76cf007afc580d3b9162b24edab00a279f65b
Vanessa Charles Thompson <vane.charles24@gmail.com>
2020-01-27 11:19:09 -0500

    [NO-TICKET] more celery config

--- nabi_api_django/celery_config.py
+++ nabi_api_django/celery_config.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nabi_api_django.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nabi_api_django.production_settings')
 
 app = Celery('my_tasks')
 app.config_from_object('django.conf:settings', namespace='CELERY')
