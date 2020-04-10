4a644b14a536650a3d9eabd2503544af24c619df
Luis Llave <llaveluis@gmail.com>
2020-04-09 21:57:43 -0500

    [NM-242] Update admin interface to enable add (create) User

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,86 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from core.constants import BENEFIT_PENDING
+from accounts.serializers import (InstructorCreateAccountSerializer, ParentCreateAccountSerializer,
+                                  StudentCreateAccountSerializer)
 
+from .constants import BENEFIT_PENDING, ROLE_INSTRUCTOR, ROLE_PARENT, ROLE_STUDENT
+from .forms import CreateUserForm
 from .models import UserBenefits
+from .utils import generate_random_password
+
+User = get_user_model()
+
+
+class ProfileListFilter(admin.SimpleListFilter):
+    title = 'Profile Type'
+    parameter_name = 'profile'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('instructor', 'Instructor'),
+            ('parent', 'Parent'),
+            ('student', 'Student'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'instructor':
+            return queryset.filter(instructor__isnull=False)
+        if self.value() == 'parent':
+            return queryset.filter(parent__isnull=False)
+        if self.value() == 'student':
+            return queryset.filter(student__isnull=False)
+
+
+class UserAdmin(admin.ModelAdmin):
+    """Copied from django.contrib.auth.admin.UserAdmin"""
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'profile',)}),
+        ('Permissions', {'fields': ('is_superuser', 'is_active',)}),
+        ('Refer info', {'fields': ('referral_token', 'referred_by',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name',)}),
+        ('Additional data', {'fields': ('role', 'birthday', 'referringCode',)}),
+    )
+    list_display = ('pk', 'email', 'first_name', 'last_name', 'profile', 'is_active',)
+    list_filter = ('is_active', ProfileListFilter,)
+    search_fields = ('email', 'first_name', 'last_name',)
+    readonly_fields = ('profile', 'referral_token', 'referred_by',)
+    ordering = ('pk',)
+    add_form = CreateUserForm
+
+    def profile(self, instance):
+        return instance.get_role()
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during user creation
+        """
+        defaults = {}
+        if obj is None:
+            defaults['form'] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+    def save_model(self, request, obj, form, change):
+        role = form.cleaned_data.pop('role')
+        form.cleaned_data['password'] = generate_random_password(10)
+        if role == ROLE_INSTRUCTOR:
+            ser = InstructorCreateAccountSerializer(data=form.cleaned_data)
+        if role == ROLE_PARENT:
+            ser = ParentCreateAccountSerializer(data=form.cleaned_data)
+        if role == ROLE_STUDENT:
+            ser = StudentCreateAccountSerializer(data=form.cleaned_data)
+        if ser.is_valid():
+            ser.save()
+        else:
+            raise Exception(f'{ser.errors}')
 
 
 class BuggedDependentBenefitFilter(admin.SimpleListFilter):
@@ -30,4 +108,5 @@ class UserBenefitsAdmin(admin.ModelAdmin):
     search_fields = ('beneficiary__email', 'provider__email')
 
 
+admin.site.register(User, UserAdmin)
 admin.site.register(UserBenefits, UserBenefitsAdmin)
