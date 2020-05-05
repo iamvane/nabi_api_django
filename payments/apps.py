0bc7dd3700eec4f072ed1a05cc305a86f9550210
Luis Llave <llaveluis@gmail.com>
2020-05-05 12:08:30 -0500

    [NM-265] Change models for payments, because Stripe's payment method id won't be stored

--- payments/apps.py
+++ payments/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class PaymentsConfig(AppConfig):
     name = 'payments'
-
-    def ready(self):
-        from . import signals
--- payments/models.py
+++ payments/models.py
@@ -6,20 +6,11 @@ from core.constants import PY_REGISTERED, PY_STATUSES
 User = get_user_model()
 
 
-class UserPaymentMethod(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payment_methods')
-    stripe_payment_method_id = models.CharField(max_length=200)
-    is_main = models.BooleanField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
-    payment_method = models.ForeignKey(UserPaymentMethod, blank=True, null=True,
-                                       on_delete=models.SET_NULL, related_name='charges')
     amount = models.DecimalField(max_digits=9, decimal_places=4)
     description = models.CharField(max_length=300)
+    stripe_payment_method = models.CharField(max_length=200)
     operation_id = models.CharField(max_length=300)
     status = models.CharField(max_length=100, choices=PY_STATUSES, default=PY_REGISTERED)
     payment_date = models.DateTimeField(auto_now_add=True)
