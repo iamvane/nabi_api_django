7566020b3771503166fb6a8a71eb6c59e8f39523
Luis Llave <llaveluis@gmail.com>
2020-07-08 11:00:51 -0500

    [NM-339] Update verification of previous booking, to allow trial lesson for each TiedStudent. Fix error in returned data, to include lessonId

--- accounts/views.py
+++ accounts/views.py
@@ -559,14 +559,18 @@ class StudentView(views.APIView):
         if serializer.is_valid():
             student_details = serializer.save()
             ser = sers.StudentSerializer(student_details)
-            if request.user.lesson_bookings.count() == 0:
-                if request.user.is_parent():
+            trial_lesson = None
+            if request.user.is_parent():
+                if request.user.lesson_bookings.filter(tied_student=student_details.tied_student).count() == 0:
                     trial_lesson = LessonBooking.create_trial_lesson(request.user,
                                                                      tied_student=student_details.tied_student)
-                else:
+            else:
+                if request.user.lesson_bookings.count() == 0:
                     trial_lesson = LessonBooking.create_trial_lesson(request.user)
-                ser.data['lesson_id'] = trial_lesson.id
-            return Response(ser.data, status=status.HTTP_200_OK)
+            return_data = ser.data.copy()
+            if trial_lesson:
+                return_data['lessonId'] = trial_lesson.id
+            return Response(return_data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
