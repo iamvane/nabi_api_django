3a8bd5bc7688e78c0441b4e8d278bdbf2beb2e24
Luis Llave <llaveluis@gmail.com>
2020-08-18 14:15:21 -0500

    [NM-368] Update code in admin site for LessonBooking edition, to call tasks for send alert email to student/parent and instructor

--- lesson/admin.py
+++ lesson/admin.py
@@ -5,14 +5,14 @@ from django.db.models import Q
 
 from accounts.models import Instructor, TiedStudent
 from accounts.utils import add_to_email_list_v2
-from core.constants import LESSON_REQUEST_CLOSED, PY_APPLIED
+from core.constants import LESSON_REQUEST_ACTIVE, LESSON_REQUEST_CLOSED, PY_APPLIED
 from core.models import TaskLog, UserBenefits
 from lesson.models import Instrument
 from payments.models import Payment
 
 from .models import Application, Lesson, LessonBooking, LessonRequest
 from .tasks import (send_application_alert, send_booking_alert, send_booking_invoice, send_info_grade_lesson,
-                    send_request_alert_instructors, send_lesson_info_student_parent)
+                    send_request_alert_instructors, send_lesson_info_student_parent, send_lesson_info_instructor)
 
 User = get_user_model()
 
@@ -114,6 +114,28 @@ class LessonBookingAdmin(admin.ModelAdmin):
                 if obj.application:
                     task_log = TaskLog.objects.create(task_name='send_booking_alert', args={'booking_id': obj.id})
                     send_booking_alert.delay(obj.id, task_log.id)
+        elif 'instructor' in form.changed_data:
+            if obj.instructor:
+                with transaction.atomic():
+                    if 'rate' not in form.changed_data:
+                        rate_obj = obj.instructor.instructorlessonrate_set.last()
+                        obj.rate = rate_obj.mins30
+                        obj.save()
+                        obj.refresh_from_db()
+                    if obj.status == LessonBooking.TRIAL:
+                        if obj.request and obj.request.status == LESSON_REQUEST_ACTIVE:
+                            obj.request.status = LESSON_REQUEST_CLOSED
+                            obj.request.save()
+                            lesson = obj.lessons.first()
+                            lesson.instructor = obj.instructor
+                            lesson.rate = obj.rate
+                            lesson.save()
+                        task_log = TaskLog.objects.create(task_name='send_lesson_info_student_parent',
+                                                          args={'lesson_id': lesson.id})
+                        send_lesson_info_student_parent.delay(lesson.id, task_log.id)
+                        task_log = TaskLog.objects.create(task_name='send_lesson_info_instructor',
+                                                          args={'lesson_id': lesson.id})
+                        send_lesson_info_instructor.delay(lesson.id, task_log.id)
 
 
 def close_lesson_request(model_admin, request, queryset):
