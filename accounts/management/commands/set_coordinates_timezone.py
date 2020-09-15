1876b32de6ddff0273946948c9f1562ee652f6d1
Luis <llaveluis@gmail.com>
2020-09-14 21:55:40 -0500

    [NM-416] Coordinates timezone admin
    * [NM-416] Update code in admin site for set coordinates and timezone when location is set
    
    * [NM-416] Command to set coordinates and timezone for existing users

--- accounts/management/commands/set_coordinates_timezone.py
+++ accounts/management/commands/set_coordinates_timezone.py
@@ -2,14 +2,15 @@ from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
 from accounts.models import get_account
+from accounts.utils import get_geopoint_from_location
 
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    """Set timezone value, from coordinates or location (zip code)"""
+    """Set coordinates from location, set timezone from coordinates or location (zip code)"""
     args = ''
-    help = 'Set timezone, from coordinates or location (zip code)'
+    help = 'Set coordinates and timezone values'
 
     def handle(self, *args, **options):
         self.stdout.write('Start update process')
@@ -17,6 +18,11 @@ class Command(BaseCommand):
         for user in User.objects.all():
             account = get_account(user)
             if account:
+                if not account.location:
+                    self.stdout.write(f'{user.email} has not location')
+                    continue
+                account.coordinates = get_geopoint_from_location(account.location)
+                account.save()
                 time_zone = account.get_timezone_from_location_zipcode(default_value='')
                 if time_zone:
                     account.timezone = time_zone
