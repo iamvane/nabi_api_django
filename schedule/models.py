6ec222706609a94f27ada4316e076c6b2a6d63ad
Luis Llave <llaveluis@gmail.com>
2020-10-13 14:29:45 -0500

    [NM-438] Update models for store availability data of an instructor

--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 from core.utils import DayChoices
 
@@ -6,15 +7,11 @@ from core.utils import DayChoices
 class InstructorRegularAvailability(models.Model):
     instructor = models.ForeignKey('accounts.Instructor', on_delete=models.CASCADE, related_name='regular_availability')
     week_day = models.IntegerField(choices=DayChoices.choices)
-    begin_time = models.TimeField()
-    end_time = models.TimeField()
-    available = models.BooleanField()
+    schedule = JSONField()
 
 
 class InstructorParticularAvailability(models.Model):
     instructor = models.ForeignKey('accounts.Instructor', on_delete=models.CASCADE,
                                    related_name='particular_availability')
     date = models.DateField()
-    begin_time = models.TimeField()
-    end_time = models.TimeField()
-    available = models.BooleanField()
+    schedule = JSONField()
