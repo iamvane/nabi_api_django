e510d7c0f2b60a03973416ed9ee8bfc2e60ca81f
Luis Llave <llaveluis@gmail.com>
2020-10-13 17:27:41 -0500

    [NM-438] Add admin for availability models of schedule app

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import InstructorParticularAvailability, InstructorRegularAvailability
+
+
+class InstructorParticularAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('instructor', 'date', )
+    fields = ('instructor', 'date', 'schedule')
+    search_fields = ('instructor__user__email', )
+
+    class Meta:
+        model = InstructorParticularAvailability
+
+
+class InstructorRegularAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('instructor', 'week_day', )
+    fields = ('instructor', 'week_day', 'schedule')
+    search_fields = ('instructor__user__email',)
+
+    class Meta:
+        model = InstructorRegularAvailability
+
+
+admin.site.register(InstructorParticularAvailability, InstructorParticularAvailabilityAdmin)
+admin.site.register(InstructorRegularAvailability, InstructorRegularAvailabilityAdmin)
