e741ba21373a47931672f5a3947e5d658404d2a8
Luis Llave <llaveluis@gmail.com>
2020-10-28 21:46:25 -0500

    [NM-438] Update code for return 00 as hours in days without registered availability

--- schedule/utils.py
+++ schedule/utils.py
@@ -6,5 +6,5 @@ def compose_schedule_data(instructor, this_date, orig_data):
         # first, order received schedule data
         data = sorted(orig_data, key=lambda item: item.get('beginTime'))
     else:
-        data = [{'beginTime': '08:00', 'endTime': '20:00', 'available': False}]
+        data = [{'beginTime': '00:00', 'endTime': '00:00', 'available': False}]
     return data
