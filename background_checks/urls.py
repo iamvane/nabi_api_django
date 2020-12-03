762499256eedef3698854147d806f6f801c3f060
Luis Llave <llaveluis@gmail.com>
2020-12-02 21:15:38 -0500

    [NM-451] Remove code for background checks endpoint, and related models

--- background_checks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('background-check-request/', views.BackgroundCheckRequestView.as_view(), name='background-checks-request'),
-    path('background-check-status/', views.BackgroundCheckStatusView.as_view(), name='background-checks-status'),
-]
